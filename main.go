f93934b28496b700eac25ed4e9053eb65443f483
Benjamin Cable <cable.benjamin@gmail.com>
2015-11-24 19:20:33 +0100

    rebase later
    Signed-off-by: Benjamin Cable <cable.benjamin@gmail.com>

--- main.go
+++ main.go
@@ -4,11 +4,38 @@ import (
 	"time"
 )
 
+const (
+	// PomodoroLength defines the default length of a pomodoro
+	// should be later sourced via .termodororc
+	PomodoroLength time.Duration = 25
+)
+
 func main() {
 
 }
 
-func GetCurrentTime() (t time.Time) {
+// Pomodoro interface implements getters and setters for active pomodoro
+type Pomodoro struct {
+	currentTime time.Time
+	pomodoroEnd time.Time
+}
+
+// GetCurrentTime gets the current time
+// Implements the Pomodoro struct
+func (p *Pomodoro) GetCurrentTime() (t time.Time) {
 	t = time.Now()
+	p.currentTime = t
 	return
+}
+
+// AddPomodoro calculates when the next Pomodoro should end
+func (p *Pomodoro) AddPomodoro(t time.Time) (pomodoro time.Time) {
+	pomodoro = t.Add(PomodoroLength * time.Minute)
+	p.pomodoroEnd = pomodoro
+	return
+}
+
+// TickTilPomodoroEnd counts down the time until active pomodoro ends
+func (p *Pomodoro) TickTilPomodoroEnd(pomodoro time.Time) {
+	// time.Ticker ??
 }
--- main_test.go
+++ main_test.go
@@ -1,12 +1,22 @@
 package main
 
-import (
-	"testing"
-	// "time"
+import "testing"
+
+const (
+	layout string = "Jan 1 2006 at 15:04:01"
 )
 
-func TestGetCurrentTime(t *testing.T) {
-	layout := "Jan 1 2006 at 3:04pm"
-	ti := GetCurrentTime().Format(layout)
+func (p *Pomodoro) TestGetCurrentTime(t *testing.T) {
+	ti := p.GetCurrentTime().Format(layout)
 	t.Log(ti)
+}
+
+func (p *Pomodoro) TestAddPomodoro(t *testing.T) {
+	ti := p.GetCurrentTime()
+	pomodoro := p.AddPomodoro(ti)
+	t.Log(pomodoro)
+}
+
+func TestTickTilPomodoroEnd(t *testing.T) {
+	// time.Ticker() ??
 }
